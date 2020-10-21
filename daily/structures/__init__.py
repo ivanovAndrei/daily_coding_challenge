583c3f8f643c31088c055835feed5820e04bc7ca
ivanovAndrei <ivanov.andrei@gmail.com>
2020-10-20 20:18:57 -0600

    pytest

--- daily/structures/__init__.py
+++ daily/structures/__init__.py
@@ -1,3 +1,3 @@
 from daily.structures.binary_tree import Node
-from daily.structures.elastic_index import ElasticIndex
+from daily.structures.elastic_index import SearchIndex
 from daily.structures.string_decoder import StringDecoder
